a35cc3d6128537c25fd5d1442d7de8a026c709f8
Geunyoung Kim <kgysmart@gmail.com>
2018-11-05 14:27:10 -0800

    Add Getting Under the Hood

--- slices/slices.go
+++ slices/slices.go
@@ -3,9 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	// Declares a slice called myCourses
-	myCourses := []string{"Docker", "Puppet", "Python"}
-	// myCourses := make([]string, 5, 10)
+	/*
+		//// Slice syntax
+		// Declares a slice called myCourses
+		myCourses := []string{"Docker", "Puppet", "Python"}
+		// myCourses := make([]string, 5, 10)
 
-	fmt.Printf("Length is: %d.\nCapacity is: %d", len(myCourses), cap(myCourses))
+		fmt.Printf("Length is: %d.\nCapacity is: %d", len(myCourses), cap(myCourses))
+	*/
+
+	mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(mySlice[4])
+
+	mySlice[1] = 0
+	fmt.Println(mySlice)
+
+	sliceOfSlice := mySlice[2:5]
+	fmt.Println(sliceOfSlice)
+
 }
