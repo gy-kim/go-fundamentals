c60ccb24a70b1e87031149fdb211507917122e55
Geunyoung Kim <kgysmart@gmail.com>
2018-11-06 09:06:33 -0800

    Add Manipulating Maps

--- maps/maps.go
+++ maps/maps.go
@@ -3,14 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	leagueTitles := make(map[string]int)
-	leagueTitles["Sunderland"] = 6
-	leagueTitles["Newcastle"] = 4
 
-	recentHead2Head := map[string]int{
-		"Sunderland": 5,
-		"Newcastle":  0,
+	/*
+		//// Maps Syntax
+		leagueTitles := make(map[string]int)
+		leagueTitles["Sunderland"] = 6
+		leagueTitles["Newcastle"] = 4
+
+		recentHead2Head := map[string]int{
+			"Sunderland": 5,
+			"Newcastle":  0,
+		}
+
+		fmt.Printf("\nLeague titles: %v\nRecent head to heads: %v\n", leagueTitles, recentHead2Head)
+	*/
+
+	/* */
+	//// Manipulating Maps
+	testMap := map[string]int{"A": 1, "B": 2, "C": 3, "D": 4, "E": 5}
+
+	for key, value := range testMap {
+		fmt.Printf("\nKey is: %v Value is: %v", key, value)
 	}
 
-	fmt.Printf("\nLeague titles: %v\nRecent head to heads: %v\n", leagueTitles, recentHead2Head)
+	testMap["A"] = 100
+	testMap["F"] = 1973
+	fmt.Println("\n", testMap)
+
+	delete(testMap, "F")
+	fmt.Println(testMap)
 }
