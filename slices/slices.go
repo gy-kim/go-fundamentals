ab4747077ac4e68094fead85e80bdfa6e1bb7f74
Geunyoung Kim <kgysmart@gmail.com>
2018-11-06 08:40:45 -0800

    Add Append

--- slices/slices.go
+++ slices/slices.go
@@ -5,20 +5,33 @@ import "fmt"
 func main() {
 	/*
 		//// Slice syntax
-		// Declares a slice called myCourses
 		myCourses := []string{"Docker", "Puppet", "Python"}
 		// myCourses := make([]string, 5, 10)
 
 		fmt.Printf("Length is: %d.\nCapacity is: %d", len(myCourses), cap(myCourses))
 	*/
 
-	mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	fmt.Println(mySlice[4])
+	/*
+		//// Getting Under the Hood
+		mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		fmt.Println(mySlice[4])
 
-	mySlice[1] = 0
-	fmt.Println(mySlice)
+		mySlice[1] = 0
+		fmt.Println(mySlice)
 
-	sliceOfSlice := mySlice[2:5]
-	fmt.Println(sliceOfSlice)
+		sliceOfSlice := mySlice[2:5]
+		fmt.Println(sliceOfSlice)
+
+	*/
+
+	/* */
+	//// Append
+	mySlice := make([]int, 1, 4)
+	fmt.Printf("Length is: %d Capacity is: %d", len(mySlice), cap(mySlice))
+
+	for i := 1; i < 17; i++ {
+		mySlice = append(mySlice, i)
+		fmt.Printf("\nCapacity is: %d", cap(mySlice))
+	}
 
 }
